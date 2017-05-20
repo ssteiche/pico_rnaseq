9598bfe97815ad4f3d4fdc396fb8258cb9203152
David Astling <david.astling@ucdenver.edu>
2017-05-19 23:51:02 -0600

    add trinity script
    - process the combined reads from the temperature experiment
    - hopefully better coverage than with a single file
    - bam file has unaligned reads

---
+++ code/trinity.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#BSUB -J trinity
+#BSUB -o logs/trinity_%J.out
+#BSUB -e logs/trinity_%J.err
+#BSUB -R "select[mem>60] rusage[mem=60] span[hosts=1]"
+#BSUB -n 12
+#BSUB -P llaurens
+
+# catch unset variables, non-zero exits in pipes and calls, enable x-trace.
+set -o nounset -o pipefail -o errexit -x
+
+Trinity \
+--genome_guided_bam alignments/temperature_expt_sorted.bam \
+--max_memory 50G \
+--genome_guided_max_intron 10000 \
+--genome_guided_min_coverage 50 \
+--genome_guided_min_reads_per_partition 50 \
+--CPU 12 \
+--output trinity_assembly
+
