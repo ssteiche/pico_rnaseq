7e237d0bb88130ee117c911db75a428650aa33d7
David Astling <david.astling@ucdenver.edu>
2017-05-19 23:57:55 -0600

    refactor gsnap genome build script
    - move settings to the config script so that changes can be made to a
      single file

--- code/build_gsnap_genome.sh
+++ code/build_gsnap_genome.sh
@@ -9,22 +9,17 @@
 # catch unset variables, non-zero exits in pipes and calls, enable x-trace.
 set -o nounset -o pipefail -o errexit -x
 
-genome_name=se107_orig
-GSNAP_DIR=$HOME/bin/gsnap_2017-05-08/bin
-MAPDIR=$HOME/gmapdb
-fasta=$HOME/Projects/llaurens/se107/assembly/SE107.scaffolds.fa
-#gtf_ref=$HOME/Projects/llaurens/se107/annotation/SE107_augustus_gene_predictions.gtf
-gtf_ref=genome/se107_augustus_exons.gtf
+source code/config.sh
 
-$GSNAP_DIR/gmap_build -d $genome_name $fasta
+$GSNAP_DIR/gmap_build -d $SPECIES $fasta
 
-cat $gtf_ref | $GSNAP_DIR/gtf_splicesites > $genome_name.splicesites
-cat $gtf_ref | $GSNAP_DIR/gtf_introns > $genome_name.introns
-cat $genome_name.splicesites | $GSNAP_DIR/iit_store -o $genome_name.splicesites.iit
-cat $genome_name.introns | $GSNAP_DIR/iit_store -o $genome_name.introns.iit
+cat $gtf_ref | $GSNAP_DIR/gtf_splicesites > $SPECIES.splicesites
+cat $gtf_ref | $GSNAP_DIR/gtf_introns > $SPECIES.introns
+cat $SPECIES.splicesites | $GSNAP_DIR/iit_store -o $SPECIES.splicesites.iit
+cat $SPECIES.introns | $GSNAP_DIR/iit_store -o $SPECIES.introns.iit
 
-mv $genome_name.splicesites $MAPDIR/$genome_name
-mv $genome_name.introns $MAPDIR/$genome_name
-mv $genome_name.splicesites.iit $MAPDIR/$genome_name/$genome_name.maps
-mv $genome_name.introns.iit $MAPDIR/$genome_name/$genome_name.maps
+mv $SPECIES.splicesites $MAPDIR/$SPECIES
+mv $SPECIES.introns $MAPDIR/$SPECIES
+mv $SPECIES.splicesites.iit $MAPDIR/$SPECIES/$SPECIES.maps
+mv $SPECIES.introns.iit $MAPDIR/$SPECIES/$SPECIES.maps
 
