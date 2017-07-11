f464ad0259569010475a6609f4db804d61f1056c
David Astling <dastling@gmail.com>
2017-07-11 16:20:37 -0600

    complete time series approaches in limma script
    - version of script used for Lieve's LDRD presentation

--- code/limma.R
+++ code/limma.R
@@ -9,63 +9,19 @@ library(limma)
 source("code/ballgown.R")
 source("code/load_annotation.R")
 
-###################################################################
-# Create a simple model looking for differences between treatments
-###################################################################
-x <- dplyr::filter(pheno_data, type.of.stress == "abiotic")
-X.sub = X[x[["sample"]], ]
-X.sub = t(X.sub)
-X.sub = log2(X.sub + 1)
-
-treatment <- factor(x[["treatment"]])
-time      <- x[["sampling.time.in.hrs"]]
-
-library(splines)
-time = ns(time, df = 1)
-
-design <- model.matrix(~ treatment * time)
-colnames(design) <- c("intercept", "cold", "hot", "time", "cold:time", "hot:time")
-fit <- lmFit(X.sub, design)
-fit <- eBayes(fit)
-
-hot.specific  = topTable(fit, coef = "hot", adjust = "BH", number = nrow(X.sub))
-cold.specific = topTable(fit, coef = "cold", adjust = "BH", number = nrow(X.sub))
-top.stress    = topTable(fit, coef = 5:6, adjust = "BH", number = nrow(X.sub))
-
-
-#####################################################
-# Follow the instructions using a natural spline
-#####################################################
-x <- dplyr::filter(pheno_data, type.of.stress == "abiotic")
-X.sub = X[x[["sample"]], ]
-X.sub = t(X.sub)
-X.sub = log2(X.sub + 1)
-
-treatment <- factor(x[["treatment"]])
-time      <- x[["sampling.time.in.hrs"]]
-
-library(splines)
-time = ns(time, df = 4)
-
-design <- model.matrix(~ treatment * time)
-fit <- lmFit(X.sub, design)
-fit <- eBayes(fit)
-
-hot.specific  = topTable(fit, coef = c(8,10,12,14), adjust = "BH", number = nrow(X.sub))
-cold.specific = topTable(fit, coef = c(9,11,13,15), adjust = "BH", number = nrow(X.sub))
-top.stress    = topTable(fit, coef = 8:15, adjust = "BH", number = nrow(X.sub))
-
-
-
+# TODO: come up with a better name (this is the original expression data as a matrix) we want to convert to a data frame so it can be merged with the results files
+df = t(X)
+df = as.data.frame(df)
+df = mutate(df, gene = rownames(df))
 
 
 
 ################################################################
-# Limit analysis to the first three hours of the time course
+# Limit analysis to the first few hours of the time course
+#    - because only a few points, treat time as a categorical variable
 #    - These responses are most likely to be linear
 #    - likely identify the primary responses to the treatment
 ################################################################
-
 x <- dplyr::filter(pheno_data, type.of.stress == "abiotic", sampling.time.in.hrs <= 9) %>% arrange(treatment, sampling.time.in.hrs)
 X.sub = X[x[["sample"]], ]
 X.sub = t(X.sub)
@@ -82,29 +38,140 @@ cont.dif <- makeContrasts(
     Dif1hr = "(hot.1 - ambient.0) - (ambient.1 - ambient.0)",
     Dif3hr = "(hot.3 - hot.1) - (ambient.3 - ambient.1)",
     Dif9hr = "(hot.9 - hot.3) - (ambient.9 - ambient.3)",
-	levels = design)
+    levels = design)
 fit2 <- contrasts.fit(fit, cont.dif)
 fit2 <- eBayes(fit2)
-hot.specific = topTable(fit2, adjust = "BH", number = ncol(X.sub))
+hot.specific = topTable(fit2, adjust = "BH", number = nrow(X.sub))
 
 
 cont.dif <- makeContrasts(
     Dif1hr = "(cold.1 - ambient.0) - (ambient.1 - ambient.0)",
     Dif3hr = "(cold.3 - cold.1) - (ambient.3 - ambient.1)",
     Dif9hr = "(cold.9 - cold.3) - (ambient.9 - ambient.3)",
-	levels = design)
+    levels = design)
 fit2 <- contrasts.fit(fit, cont.dif)
 fit2 <- eBayes(fit2)
-cold.specific = topTableF(fit2, adjust = "BH", number = ncol(X.sub))
+cold.specific = topTableF(fit2, adjust = "BH", number = nrow(X.sub))
 
 
-hot.specific = mutate(hot.specific, gene = rownames(hot.specific)) %>%
-				dplyr::filter(adj.P.Val <= 0.05)
+hot.specific  = mutate(hot.specific, gene = rownames(hot.specific))
+cold.specific = mutate(cold.specific, gene = rownames(cold.specific))
 
-cold.specific = mutate(cold.specific, gene = rownames(cold.specific)) %>%
-				dplyr::filter(adj.P.Val <= 0.05)
+hot.specific  = left_join(hot.specific, df, by = "gene")
+cold.specific = left_join(cold.specific, df, by = "gene")
 
-de.genes <- unique(c(hot.specific[["gene"]], cold.specific[["gene"]]))
+output.dir = "results/initial_timepoints"
+if (! dir.exists(output.dir))
+{
+    dir.create(output.dir, recursive = TRUE)
+}
+write.table(hot.specific, file = file.path(output.dir, "hot_specific.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(cold.specific, file = file.path(output.dir, "cold_specific.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+
+###################################################################
+# Create a simple linear model looking for differences between treatments
+###################################################################
+x <- dplyr::filter(pheno_data, type.of.stress == "abiotic")
+X.sub = X[x[["sample"]], ]
+X.sub = t(X.sub)
+X.sub = log2(X.sub + 1)
+
+treatment <- factor(x[["treatment"]])
+time      <- x[["sampling.time.in.hrs"]]
+
+library(splines)
+time = ns(time, df = 1)
+
+# TODO: why not model.matrix(~ time + treatment) ??
+
+design <- model.matrix(~ treatment * time)
+colnames(design) <- c("intercept", "cold", "hot", "time", "cold:time", "hot:time")
+fit <- lmFit(X.sub, design)
+fit <- eBayes(fit)
+
+hot.specific  = topTable(fit, coef = "hot", adjust = "BH", number = nrow(X.sub))
+cold.specific = topTable(fit, coef = "cold", adjust = "BH", number = nrow(X.sub))
+top.stress    = topTable(fit, coef = 5:6, adjust = "BH", number = nrow(X.sub))
+
+hot.specific  = mutate(hot.specific, gene = rownames(hot.specific))
+cold.specific = mutate(cold.specific, gene = rownames(cold.specific))
+top.stress    = mutate(top.stress, gene = rownames(top.stress))
+
+
+hot.specific  = left_join(hot.specific, df, by = "gene")
+cold.specific = left_join(cold.specific, df, by = "gene")
+top.stress    = left_join(top.stress, df, by = "gene")
+
+output.dir = "results/simple_model"
+if (! dir.exists(output.dir))
+{
+    dir.create(output.dir, recursive = TRUE)
+}
+write.table(hot.specific, file = file.path(output.dir, "hot_specific.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(cold.specific, file = file.path(output.dir, "cold_specific.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(top.stress, file = file.path(output.dir, "top_stress.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+
+
+
+
+#####################################################
+# Follow the instructions using a natural spline
+#####################################################
+x <- dplyr::filter(pheno_data, type.of.stress == "abiotic")
+X.sub = X[x[["sample"]], ]
+X.sub = t(X.sub)
+X.sub = log2(X.sub + 1)
+
+treatment <- factor(x[["treatment"]])
+time = x[["sampling.time.in.hrs"]]
+time = cbind(
+    ns(time, df = 1),
+    dnorm(time, mean =  9,  sd = 5),
+    dnorm(time, mean = 24, sd = 12),
+    pnorm(time, mean =  5,  sd = 3)
+)
+colnames(time) = 1:ncol(time)
+y.max = apply(time, 2, max)
+time = t(apply(time, 1, function(x) { (x / y.max) * 0.8 }))
+
+design <- model.matrix(~ treatment * time)
+fit <- lmFit(X.sub, design)
+fit <- eBayes(fit)
+
+hot.specific  = topTable(fit, coef = c(8,10,12,14), adjust = "BH", number = nrow(X.sub))
+cold.specific = topTable(fit, coef = c(9,11,13,15), adjust = "BH", number = nrow(X.sub))
+top.stress    = topTable(fit, coef = 8:15, adjust = "BH", number = nrow(X.sub))
+
+hot.specific  = mutate(hot.specific, gene = rownames(hot.specific))
+cold.specific = mutate(cold.specific, gene = rownames(cold.specific))
+top.stress    = mutate(top.stress, gene = rownames(top.stress))
+
+
+hot.specific  = left_join(hot.specific, df, by = "gene")
+cold.specific = left_join(cold.specific, df, by = "gene")
+top.stress    = left_join(top.stress, df, by = "gene")
+
+output.dir = "results/full_model"
+if (! dir.exists(output.dir))
+{
+    dir.create(output.dir, recursive = TRUE)
+}
+write.table(hot.specific, file = file.path(output.dir, "hot_specific.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(cold.specific, file = file.path(output.dir, "cold_specific.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(top.stress, file = file.path(output.dir, "top_stress.txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+
+
+
+
+#----------------------------------------------
+
+# functions here for downstream plotting
+
+
 
 
 ###########################################################################
@@ -119,48 +186,14 @@ foo = left_join(foo, pheno_data, by = "sample")
 foo = rename(foo, time = sampling.time.in.hrs)
 foo = mutate(foo, treatment = factor(treatment, levels = c("hot", "ambient", "cold")))
 
-plot_gene <- function(df, gene.name)
+plot_gene <- function(df, gene.name, plot.title = gene.name)
 {
     df = filter(df, gene == gene.name)
     p = ggplot(df, aes(time, expression, color = treatment)) + 
         geom_point() +
         geom_smooth(span = 0.3) +
-        labs(title = gene.name)
+        labs(title = plot.title)
     return(p)
 }
-
-
-pdf(file = "~/Desktop/temp_curves.pdf", height = 40, width = 4)
-p = ggplot(bar, aes(time, expression, color = treatment)) + 
-    geom_point() +
-    geom_smooth(span = 0.3)
-#    facet_grid(gene ~ ., scales = "free")
-print(p)
-dev.off()
-
-
-
-
-X.sub = spread(X.sub, gene, expression)
-
-# normalize to the 0hr
-
-baseline = 
-
-# For each expt:
-#    - consider all time points as a linear effect
-#    - consider custom time points
-#    - consider first few timepoints
-#    - limit to just a few timepoints
-#    - try with temp as a numerical variable
-#    - how to set the control? (hot vs control, and cold versus control)
-
-
-# For the abiotic expt, compare:
-#    - replicate, time, temp
-
-
-# For the abiotic expt, compare:
-#    - replicate, time, treatment
 
 
