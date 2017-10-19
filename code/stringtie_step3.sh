cd70cb842f1a80eb723b1df4f1725ba4aea5350f
David Astling <david.astling@ucdenver.edu>
2017-10-18 21:54:18 -0600

    add code for re-running stringtie with merged transcripts

---
+++ code/stringtie_step3.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#BSUB -J stringtie3[1-62]%5
+#BSUB -o logs/stringtie_step3_%J.out
+#BSUB -e logs/stringtie_step3_%J.err
+#BSUB -R "select[mem>20] rusage[mem=20] span[hosts=1]"
+#BSUB -n 12
+#BSUB -P llaurens
+
+#62 samples
+
+# catch unset variables, non-zero exits in pipes and calls, enable x-trace.
+set -o nounset -o pipefail -o errexit -x
+
+source code/config.sh
+
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+bam=alignments/${sample}_sorted.bam
+
+stringtie -eB \
+-p 12 \
+-G genome/stringtie_merge.gtf \
+-m 150 \
+-c 5 \
+-o ballgown/$sample/${sample}_step3.gtf \
+$bam
+
+
